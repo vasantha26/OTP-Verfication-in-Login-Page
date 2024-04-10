8c1ec51691ec90a30efb5578226c25fc1856b295
vasantha <vasanthamani2602@gmail.com>
2024-04-10 13:47:08 +0530

    otp verification page in login-page

---
+++ app/src/main/java/com/example/otpverification/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.otpverification
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.otpverification.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+
+import com.hbb20.CountryCodePicker
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private var callbacks : PhoneAuthProvider.OnVerificationStateChangedCallbacks ?= null
+    private lateinit var authToken : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        authToken = FirebaseAuth.getInstance()
+
+
+
+//        activityMainBinding.countyCodePicker.registerCarrierNumberEditText(activityMainBinding.etMobileNumber)
+
+        activityMainBinding.etVerifyMobileNumber.setOnClickListener {
+            if ( activityMainBinding.etMobileNumber.text.toString().trim().isEmpty()){
+                Toast.makeText(this@MainActivity,"Enter Your Mobile Number" ,Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }else {
+                selectedOtp()
+            }
+
+        }
+
+
+
+        otpSend()
+
+
+
+    }
+
+    private fun selectedOtp() {
+
+
+        val options = callbacks?.let {
+            PhoneAuthOptions.newBuilder(authToken)
+                .setPhoneNumber("+91" + activityMainBinding.etMobileNumber.text.toString().trim())
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(it)
+                .build()
+        }
+        options?.let { PhoneAuthProvider.verifyPhoneNumber(it) }
+
+    }
+
+
+    private fun otpSend() {
+
+        activityMainBinding.pbProgressBar.visibility = View.VISIBLE
+        activityMainBinding.etVerifyMobileNumber.visibility = View.INVISIBLE
+
+
+        Log.d(TAG, " otpSend >>> 1$callbacks")
+
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                Log.d(TAG, "onVerificationCompleted:$credential")
+                activityMainBinding.pbProgressBar.visibility = View.GONE
+                activityMainBinding.etVerifyMobileNumber.visibility = View.VISIBLE
+
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Log.w(TAG, "onVerificationFailed", e)
+                activityMainBinding.pbProgressBar.visibility = View.GONE
+                activityMainBinding.etVerifyMobileNumber.visibility = View.VISIBLE
+
+                Toast.makeText(this@MainActivity,e.message ,Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken,
+            ) {
+                activityMainBinding.pbProgressBar.visibility = View.GONE
+                activityMainBinding.etVerifyMobileNumber.visibility = View.VISIBLE
+                Log.d(TAG, "onCodeSent:$verificationId")
+                Toast.makeText(this@MainActivity," OTP is Successfully Send." ,Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this@MainActivity ,VerifyActivity::class.java)
+                intent.putExtra("mobileNumber" ,activityMainBinding.etMobileNumber.text.toString())
+                intent.putExtra("verificationId" ,verificationId)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+    private fun checkValidity(ccp: CountryCodePicker): Boolean {
+        return ccp.isValidFullNumber
+
+    }
+}
---
+++ app/src/main/java/com/example/otpverification/VerifyActivity.kt
@@ -0,0 +1,174 @@
+package com.example.otpverification
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.otpverification.databinding.ActivityVerifyBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthProvider
+
+class VerifyActivity : AppCompatActivity() {
+
+    private lateinit var verifyOtpBinding: ActivityVerifyBinding
+
+    private lateinit var mAuth: FirebaseAuth
+
+    companion object {
+        private const val TAG = "VerifyActivity"
+        private var verificationId: String? = null
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        verifyOtpBinding = DataBindingUtil.setContentView(this, R.layout.activity_verify)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val mobile = intent.getStringExtra("mobileNumber")
+        verificationId = intent.getStringExtra("verificationId")
+
+
+        verifyOtpBinding.tvMobileNumberPass.text = String.format("+ 91 -%s", mobile)
+
+        setupOtpInputs()
+
+    }
+
+    private fun setupOtpInputs() {
+
+        verifyOtpBinding.inputCodeOne.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().trim().isNotEmpty()) {
+                    verifyOtpBinding.inputCodeTwo.requestFocus()
+                }
+            }
+        })
+
+        verifyOtpBinding.inputCodeTwo.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().trim().isNotEmpty()) {
+                    verifyOtpBinding.inputCodeThree.requestFocus()
+                }
+            }
+        })
+
+
+        verifyOtpBinding.inputCodeThree.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().trim().isNotEmpty()) {
+                    verifyOtpBinding.inputCodeFour.requestFocus()
+                }
+            }
+        })
+
+        verifyOtpBinding.inputCodeFour.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().trim().isNotEmpty()) {
+                    verifyOtpBinding.inputCodeFive.requestFocus()
+                }
+            }
+        })
+
+        verifyOtpBinding.inputCodeFive.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().trim().isNotEmpty()) {
+                    verifyOtpBinding.inputCodeSix.requestFocus()
+                }
+            }
+        })
+
+        verifyOtpBinding.tvResendButton.setOnClickListener {
+            Toast.makeText(this@VerifyActivity, "OTP Send Successfully", Toast.LENGTH_SHORT).show()
+        }
+
+        verificationClick()
+
+
+    }
+
+    private fun verificationClick() {
+
+        verifyOtpBinding.pbVerifyProgressBar.visibility = View.VISIBLE
+        verifyOtpBinding.bvVerify.visibility = View.INVISIBLE
+
+
+        verifyOtpBinding.bvVerify.setOnClickListener {
+            if (verifyOtpBinding.inputCodeOne.text.toString().trim().isEmpty() ||
+                verifyOtpBinding.inputCodeTwo.text.toString().trim().isEmpty() ||
+                verifyOtpBinding.inputCodeThree.text.toString().trim().isEmpty() ||
+                verifyOtpBinding.inputCodeFour.text.toString().trim().isEmpty() ||
+                verifyOtpBinding.inputCodeFive.text.toString().trim().isEmpty() ||
+                verifyOtpBinding.inputCodeSix.text.toString().trim().isEmpty()
+            ) {
+                Toast.makeText(this@VerifyActivity, "OTP is not Valid ", Toast.LENGTH_SHORT)
+                    .show()
+
+            } else {
+
+                if (verificationId != null) {
+                    val code = verifyOtpBinding.inputCodeOne.text.toString().trim() +
+                            verifyOtpBinding.inputCodeTwo.text.toString().trim() +
+                            verifyOtpBinding.inputCodeThree.text.toString().trim() +
+                            verifyOtpBinding.inputCodeFour.text.toString().trim() +
+                            verifyOtpBinding.inputCodeFive.text.toString().trim() +
+                            verifyOtpBinding.inputCodeSix.text.toString().trim()
+
+                    val credential =
+                        verificationId?.let { PhoneAuthProvider.getCredential(it, code) }
+
+                    credential?.let { it1 ->
+                        FirebaseAuth.getInstance().signInWithCredential(it1)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+
+                                    verifyOtpBinding.pbVerifyProgressBar.visibility = View.VISIBLE
+                                    verifyOtpBinding.bvVerify.visibility = View.INVISIBLE
+
+                                    Toast.makeText(
+                                        this@VerifyActivity,
+                                        " Welcome ...",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val intent =
+                                        Intent(this@VerifyActivity, MainPageActivity::class.java)
+                                    intent.flags =
+                                        Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    startActivity(intent)
+                                    Log.d(TAG, "signInWithCredential:success")
+
+                                } else {
+
+                                    verifyOtpBinding.pbVerifyProgressBar.visibility = View.GONE
+                                    verifyOtpBinding.bvVerify.visibility = View.VISIBLE
+
+                                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                                    Toast.makeText(
+                                        this@VerifyActivity,
+                                        "OTP is not Valid ",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
